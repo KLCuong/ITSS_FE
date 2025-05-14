855b4cdbb2b8ccaa551f5dcc11bf077cca219c80
tduck1equack <golden4gnvx@gmail.com>
2025-05-14 13:10:05 +0700

    chore(nextjs): porting to nextjs

--- itss-fontend/src/App.jsx
+++ itss-fontend/src/App.jsx
@@ -1,31 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
-import { Layout } from 'antd';
-import ContactUs from './pages/ContactUs';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import AboutUs from './pages/AboutUs';
-import AppHeader from './components/header';
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "antd";
+import ContactUs from "./pages/contactus";
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+import AboutUs from "./pages/aboutus";
+import AppHeader from "./components/header";
 
 const { Content } = Layout;
 
 function App() {
   return (
-    <Layout style={{ minHeight: '100vh' }}>
+    <Layout style={{ minHeight: "100vh" }}>
       {/* <AppHeader /> */}
-      <AppHeader/>
+      <AppHeader />
       <Content
         style={{
-          padding: '0 50px',
-          flex: 1, 
-          overflow: 'auto',
+          padding: "0 50px",
+          flex: 1,
+          overflow: "auto",
         }}
       >
         <div
           style={{
-            background: '#fff',
+            background: "#fff",
             padding: 24,
-            minHeight: '100%', 
-            boxSizing: 'border-box',
+            minHeight: "100%",
+            boxSizing: "border-box",
           }}
         >
           <Routes>
